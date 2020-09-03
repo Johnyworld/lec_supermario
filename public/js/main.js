19e119c3bf1064dcf5c786129a96ed21eda37fb8
johnyworld <johnyworld@naver.com>
2020-09-04 01:21:06 +0900

    Add drawing sky

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,22 @@
-function loadImage(url) {
-  return new Promise(resolve => {
-    const image = new Image();
-    image.addEventListener('load', () => {
-      resolve(image);
-    });
-    image.src = url;
-  });
-}
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage } from './loaders.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 loadImage('/img/tiles.png')
 .then(image => {
-  context.drawImage(image, 0, 0);
+  const sprites = new SpriteSheet(image, 16, 16);
+  sprites.define('ground', 0, 0);
+  sprites.define('sky', 3, 23);
+
+
+  for ( let x = 0; x < 25; ++x ) {
+    for ( let y = 0; y < 14; ++y ) {
+      sprites.drawTile('sky', context, x, y);
+    }
+  }
+
 });
 
 context.fillRect(0, 0, 50, 50);
