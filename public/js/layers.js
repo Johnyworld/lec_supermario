366283bb12501552dc050571e24a3ccd17bfeb67
johnyworld <johnyworld@naver.com>
2020-09-05 21:14:08 +0900

    Add Matrix class

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,20 +1,11 @@
-function drawBackground(background, context, sprites) {
-  background.ranges.forEach(([x1, x2, y1, y2]) => {
-    for ( let x = x1; x < x2; ++x ) {
-      for ( let y = y1; y < y2; ++y ) {
-        sprites.drawTile(background.tile, context, x, y);
-      }
-    }
-  })
-}
-
-export function createBackgroundLayer(backgrounds, sprites) {
+export function createBackgroundLayer(level, sprites) {
   const buffer = document.createElement('canvas');
+  const context = buffer.getContext('2d');
   buffer.width = 256;
   buffer.height = 240;
 
-  backgrounds.forEach(background=> {
-    drawBackground(background, buffer.getContext('2d'), sprites);
+  level.tiles.forEach((tile, x, y) => {
+    sprites.drawTile(tile.name, context, x, y);
   });
 
   return function drawBackgroundLayer(context) {
