55a430826fc2a8059d99a5f05b8cf78d3a6736a9
johnyworld <johnyworld@naver.com>
2020-09-06 02:54:05 +0900

    Update creating tiles and 1-1 map data

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -12,25 +12,42 @@ export function loadImage(url) {
   });
 }
 
+function loadJSON(url) {
+  return fetch(url).then(r=> r.json());
+}
+
 function createTiles(level, backgrounds) {
-  backgrounds.forEach(background=> {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-      for ( let x = x1; x < x2; ++x ) {
-        for ( let y = y1; y < y2; ++y ) {
-          level.tiles.set(x, y, {
-            name: background.tile
-          })
-        }
+
+  function applyRange(background, xStart, xLen, yStart, yLen) {
+    const xEnd = xStart + xLen;
+    const yEnd = yStart + yLen;
+    for ( let x = xStart; x < xEnd; ++x ) {
+      for ( let y = yStart; y < yEnd; ++y ) {
+        level.tiles.set(x, y, {
+          name: background.tile
+        })
       }
-    })
-  })
+    }
+  }
+
+  backgrounds.forEach(background=> {
+    background.ranges.forEach(range => {
+      if ( range.length === 4 ) {
+        const [ xStart, xLen, yStart, yLen ] = range;
+        applyRange(background, xStart, xLen, yStart, yLen);
+
+      } else if ( range.length === 2 ) {
+        const [ xStart, yStart ] = range;
+        applyRange(background, xStart, 1, yStart, 1);
+      }
+
+    });
+  });
 }
 
 export function loadLevel(name) {
   return Promise.all([
-    fetch(`levels/${name}.json`)
-    .then(r => r.json()),
-
+    loadJSON(`levels/${name}.json`),
     loadBackgroundSprites(),
   ])
   .then(([ levelSpec, backgroundSprites ]) => {
