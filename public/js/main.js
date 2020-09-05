e56a134f5e87ce1c7e2d6da5f94978e2acd5a9b9
johnyworld <johnyworld@naver.com>
2020-09-05 16:28:16 +0900

    Add character sprites and draw mario. with refactoring

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from './SpriteSheet.js';
-import { loadImage, loadLevel } from './loaders.js';
+import { loadLevel } from './loaders.js';
+import { loadBackgroundSprites, loadMarioSprite } from './sprites.js';
 
 function drawBackground(background, context, sprites) {
   background.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -11,26 +11,60 @@ function drawBackground(background, context, sprites) {
   })
 }
 
-function loadBackgroundSprites() {
-  return loadImage('/img/tiles.png')
-  .then(image => {
-    const sprites = new SpriteSheet(image, 16, 16);
-    sprites.define('ground', 0, 0);
-    sprites.define('sky', 3, 23);
-    return sprites;
-  });
+class Compositor {
+  constructor() {
+    this.layers = [];
+  }
+
+  draw(context) {
+    this.layers.forEach(layer => {
+      layer(context);
+    })
+  }
 }
 
+function createBackgroundLayer(backgrounds, sprites) {
+  const buffer = document.createElement('canvas');
+  buffer.width = 256;
+  buffer.height = 240;
+
+  backgrounds.forEach(background=> {
+    drawBackground(background, buffer.getContext('2d'), sprites);
+  });
+
+  return function drawBackgroundLayer(context) {
+    context.drawImage(buffer, 0, 0);
+  }
+}
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
+  loadMarioSprite(),
   loadBackgroundSprites(),
   loadLevel('1-1'),
 ])
-.then(([ sprites, level, ]) => {
-  level.backgrounds.forEach(background=> {
-    drawBackground(background, context, sprites);
-  })
+.then(([ marioSprite, sprites, level, ]) => {
+
+  const comp = new Compositor();
+  const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+
+  comp.layers.push(backgroundLayer);
+
+  const pos = {
+    x: 64,
+    y: 64,
+  };
+
+  function update() {
+    comp.draw(context);
+    marioSprite.draw('idle', context, pos.x, pos.y);
+    pos.x += 2;
+    pos.y += 2;
+    requestAnimationFrame(update);
+  }
+
+  update();
+
 });
