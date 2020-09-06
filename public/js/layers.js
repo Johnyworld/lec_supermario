47c2e8d44cf95730c0f1b7fa6c5e74b26620dc5b
johnyworld <johnyworld@naver.com>
2020-09-06 23:06:16 +0900

    Add redrawing background by camera moving

--- public/js/layers.js
+++ public/js/layers.js
@@ -1,15 +1,41 @@
 export function createBackgroundLayer(level, sprites) {
+  const tiles = level.tiles;
+  const resolver = level.tileCollider.tiles;
+
   const buffer = document.createElement('canvas');
   const context = buffer.getContext('2d');
-  buffer.width = 2048;
+  buffer.width = 256 + 16;
   buffer.height = 240;
 
-  level.tiles.forEach((tile, x, y) => {
-    sprites.drawTile(tile.name, context, x, y);
-  });
+  let startIndex, endIndex;
+
+  function redraw(drawFrom, drawTo) {
+    if ( drawFrom === startIndex && drawTo === endIndex ) return;
+
+    // 현재 카메라 내에 있는 타일들만 Draw 해줄 것이다.
+    startIndex = drawFrom; // ex 3
+    endIndex = drawTo; // ex 19
+
+    for (let x = startIndex; x <= endIndex; ++x) {
+      const col = tiles.grid[x];
+      if (col) {
+        col.forEach((tile, y) => {
+          sprites.drawTile(tile.name, context, x-startIndex, y);
+        });
+      }
+    }
+  }
 
   return function drawBackgroundLayer(context, camera) {
-    context.drawImage(buffer, -camera.pos.x, -camera.pos.y);
+    const bufferWidth = resolver.toIndex(camera.size.x); // 카메라 화면의 width (16칸)
+    const drawFrom = resolver.toIndex(camera.pos.x); // 카메라 시작 위치
+    const drawTo = drawFrom + bufferWidth; // 카메라 끝 위치
+    redraw(drawFrom, drawTo);
+
+    context.drawImage(buffer,
+      -camera.pos.x % 16,
+      -camera.pos.y
+    );
   }
 }
 
