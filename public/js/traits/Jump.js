407ea94bff820ea0dde47aeed3d15fc28f537c04
johnyworld <johnyworld@naver.com>
2020-09-08 21:11:44 +0900

    Fix infinity jumping

--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -4,23 +4,33 @@ export default class Jump extends Trait {
   constructor() {
     super('jump');
 
+    this.ready = false;
     this.duration = 0.5;
     this.velocity = 200;
     this.engageTime = 0;
   }
 
   start() {
-    this.engageTime = this.duration;
+    if ( this.ready ) {
+      this.engageTime = this.duration;
+    }
   }
 
   cancel() {
     this.engageTime = 0;
   }
 
+  obstruct(entity, side) {
+    if ( side === 'bottom' ) {
+      this.ready = true;
+    }
+  }
+  
   update (entity, deltaTime) {
     if ( this.engageTime > 0 ) {
       entity.vel.y = -this.velocity;
       this.engageTime -= deltaTime;
     }
+    this.ready = false;
   }
 }
