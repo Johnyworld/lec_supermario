d443bc871825a204430f6a9d5b3a38df5a5fc909
johnyworld <johnyworld@naver.com>
2020-09-08 21:23:53 +0900

    Fix not falling when mario's head reaches to top blocks jumping

--- public/js/traits/Jump.js
+++ public/js/traits/Jump.js
@@ -4,14 +4,18 @@ export default class Jump extends Trait {
   constructor() {
     super('jump');
 
-    this.ready = false;
+    this.ready = 0;
     this.duration = 0.5;
     this.velocity = 200;
     this.engageTime = 0;
   }
 
+  get falling() {
+    return this.ready < 0;
+  }
+
   start() {
-    if ( this.ready ) {
+    if ( this.ready > 0 ) {
       this.engageTime = this.duration;
     }
   }
@@ -22,15 +26,18 @@ export default class Jump extends Trait {
 
   obstruct(entity, side) {
     if ( side === 'bottom' ) {
-      this.ready = true;
+      this.ready = 1;
+    } else if ( side === 'top' ) {
+      this.cancel();
     }
   }
   
   update (entity, deltaTime) {
+    console.log(this.ready)
     if ( this.engageTime > 0 ) {
       entity.vel.y = -this.velocity;
       this.engageTime -= deltaTime;
     }
-    this.ready = false;
+    this.ready--;
   }
 }
