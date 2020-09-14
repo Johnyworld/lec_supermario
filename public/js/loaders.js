a73f28ac32c1141e279ddf04b7d18b2a28938f40
johnyworld <johnyworld@naver.com>
2020-09-15 01:27:22 +0900

    Refactory code of loaders

--- public/js/loaders.js
+++ public/js/loaders.js
@@ -1,6 +1,4 @@
-import Level from './Level.js';
 import SpriteSheet from './SpriteSheet.js';
-import { createBackgroundLayer, createSpriteLayer } from './layers.js'
 import { createAnim } from './anim.js';
 
 export function loadImage(url) {
@@ -13,7 +11,7 @@ export function loadImage(url) {
   });
 }
 
-function loadJSON(url) {
+export function loadJSON(url) {
   return fetch(url).then(r=> r.json());
 }
 
@@ -54,68 +52,5 @@ export function loadSpriteSheet(name) {
     }
 
     return sprites;
-  });
-}
-
-
-export function loadLevel(name) {
-  return loadJSON(`levels/${name}.json`) 
-  .then(levelSpec => Promise.all([
-    levelSpec,
-    loadSpriteSheet(levelSpec.spriteSheet),
-  ]))
-  .then(([ levelSpec, backgroundSprites ]) => {
-    const level = new Level();
-
-    createTiles(level, levelSpec.backgrounds, levelSpec.patterns);
-
-    const backgroundLayer = createBackgroundLayer(level, backgroundSprites);
-    level.comp.layers.push(backgroundLayer);
-
-    const spriteLayer = createSpriteLayer(level.entities);
-    level.comp.layers.push(spriteLayer);
-
-    return level;
-  })
-};
-
-
-function createTiles(level, backgrounds, patterns, offsetX=0, offsetY=0) {
-
-  function applyRange(background, xStart, xLen, yStart, yLen) {
-    const xEnd = xStart + xLen;
-    const yEnd = yStart + yLen;
-    for ( let x = xStart; x < xEnd; ++x ) {
-      for ( let y = yStart; y < yEnd; ++y ) {
-        const derivedX = x + offsetX;
-        const derivedY = y + offsetY;
-
-        if ( background.pattern ) {
-          console.log('Pattern detected', patterns[background.pattern])
-          const backgrounds = patterns[background.pattern].backgrounds;
-          createTiles(level, backgrounds, patterns, derivedX, derivedY);
-
-        } else {
-          level.tiles.set(derivedX, derivedY, {
-            name: background.tile,
-            type: background.type
-          })
-        }
-      }
-    }
-  }
-
-  backgrounds.forEach(background=> {
-    background.ranges.forEach(range => {
-      if ( range.length === 4 ) {
-        const [ xStart, xLen, yStart, yLen ] = range;
-        applyRange(background, xStart, xLen, yStart, yLen);
-
-      } else if ( range.length === 2 ) {
-        const [ xStart, yStart ] = range;
-        applyRange(background, xStart, 1, yStart, 1);
-      }
-
-    });
   });
 }
